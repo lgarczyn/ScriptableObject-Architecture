3838c9c6e0d54254766964e4da2a09f89b5ad41a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 12:57:14 -0700

    added helper functions

--- Assets/SO Architecture/Variables/Vector3Variable.cs
+++ Assets/SO Architecture/Variables/Vector3Variable.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace ScriptableObjectArchitecture
 {
-    [System.Serializable]
-    public class Vector3Event : UnityEvent<Vector3> { }
-
     [CreateAssetMenu(
         fileName = "Vector3Variable.asset",
         menuName = SOArchitecture_Utility.VARIABLE_SUBMENU + "Structs/Vector3",
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 11)]
-    public sealed class Vector3Variable : BaseVariable<Vector3, Vector3Event>
+    public sealed class Vector3Variable : BaseVariable<Vector3>
     {
+        public void Add(Vector3 x) => Value += x;
+        public void Subtract(Vector3 x) => Value -= x;
+        public void Multiply(float x) => Value *= x;
+        public void Divide(float x) => Value /= x;
     } 
 }
