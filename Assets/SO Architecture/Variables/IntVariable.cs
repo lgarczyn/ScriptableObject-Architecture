fd1c87cf722fcfea8f75037abbff0a871ac86112
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 12:57:23 -0700

    added common helper functions

--- Assets/SO Architecture/Variables/IntVariable.cs
+++ Assets/SO Architecture/Variables/IntVariable.cs
@@ -1,16 +1,12 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
 
 namespace ScriptableObjectArchitecture
 {
-    [System.Serializable]
-    public class IntEvent : UnityEvent<int> { }
-
     [CreateAssetMenu(
         fileName = "IntVariable.asset",
         menuName = SOArchitecture_Utility.VARIABLE_SUBMENU + "int",
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 4)]
-    public class IntVariable : BaseVariable<int, IntEvent>
+    public class IntVariable : BaseVariable<int>
     {
         public override bool Clampable { get { return true; } }
         protected override int ClampValue(int value)
@@ -28,5 +24,21 @@ namespace ScriptableObjectArchitecture
                 return value;
             }
         }
+        public bool IsAtMaxValue { get => IsClamped && Value == MaxClampValue; }
+        public bool IsAtMinValue { get => IsClamped && Value == MinClampValue; }
+
+        public void Add(int x) => Value += x;
+        public void Add(IntVariable x) => Value += x;
+        public void Add(FloatVariable x) => Value += (int)x;
+
+        public void Subtract(int x) => Value -= x;
+        public void Subtract(IntVariable x) => Value -= x;
+        public void Subtract(FloatVariable x) => Value -= (int)x;
+
+        public void Multiply(int x) => Value *= x;
+        public void Divide(int x) => Value /= x;
+        public void Halve() => Value /= 2;
+        public void Double() => Value *= 2;
+        public void Negate() => Value *= -1;
     } 
 }
