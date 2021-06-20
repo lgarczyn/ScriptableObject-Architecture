925852b657a3011e938ca3ffd39b259e9d619f8c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 13:27:36 -0700

    Update LayerMaskVariable.cs

--- Assets/SO Architecture/Variables/LayerMaskVariable.cs
+++ Assets/SO Architecture/Variables/LayerMaskVariable.cs
@@ -3,14 +3,11 @@ using UnityEngine.Events;
 
 namespace ScriptableObjectArchitecture
 {
-    [System.Serializable]
-    public class LayerMaskEvent : UnityEvent<LayerMask> { }
-
 	[CreateAssetMenu(
 	    fileName = "LayerMaskVariable.asset",
 	    menuName = SOArchitecture_Utility.ADVANCED_VARIABLE_SUBMENU + "LayerMask",
 	    order = 120)]
-	public class LayerMaskVariable : BaseVariable<LayerMask, LayerMaskEvent>
+	public class LayerMaskVariable : BaseVariable<LayerMask>
 	{
 	}
 }
