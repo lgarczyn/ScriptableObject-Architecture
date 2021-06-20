a59040cda630ab510ef6fb6619272dcf0b96d36b
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 12:59:20 -0700

    reverted to forked version

--- Assets/SO Architecture/Utility/SOArchitecture_Settings.cs
+++ Assets/SO Architecture/Utility/SOArchitecture_Settings.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace ScriptableObjectArchitecture
 {
@@ -22,90 +19,50 @@ namespace ScriptableObjectArchitecture
 
         private static SOArchitecture_Settings GetInstance()
         {
-#if UNITY_EDITOR
-            SOArchitecture_Settings instance = FindInstanceInProject();
+            SOArchitecture_Settings instance = Resources.Load<SOArchitecture_Settings>("SOArchitecture_Settings");
 
             if (instance == null)
                 return CreateInstance();
 
             return instance;
-#else
-            return null;
-#endif
-        }
-        private static SOArchitecture_Settings FindInstanceInProject()
-        {
-#if UNITY_EDITOR
-            string[] settingsGUIDs = AssetDatabase.FindAssets(AssetDatabaseSearchString);
-
-            if(settingsGUIDs.Length == 0)
-            {
-                return null;
-            }
-            else if(settingsGUIDs.Length > 1)
-            {
-                Debug.LogWarning("Found more than one instance of SOArchitecture_Settings, you've probably created several SOA settings objects." +
-                    $"\nTo find all instances, type {AssetDatabaseSearchString} into the project view search bar");
-                return null;
-            }
-            else
-            {
-                string settingsPath = AssetDatabase.GUIDToAssetPath(settingsGUIDs[0]);
-                return AssetDatabase.LoadAssetAtPath<SOArchitecture_Settings>(settingsPath);
-            }
-#else
-            throw new System.NullReferenceException();
-#endif
         }
         private static SOArchitecture_Settings CreateInstance()
         {
 #if UNITY_EDITOR
-            SOArchitecture_Settings newSettings = CreateInstance<SOArchitecture_Settings>();
+            SOArchitecture_Settings newSettings = SOArchitecture_Settings.CreateInstance<SOArchitecture_Settings>();
 
-            AssetDatabase.CreateAsset(newSettings, DefaultNewSettingsLocation + DefaultNewSettingsName);
-            AssetDatabase.SaveAssets();
+            if (!UnityEditor.AssetDatabase.IsValidFolder("Assets/Resources"))
+                UnityEditor.AssetDatabase.CreateFolder("Assets", "Resources");
 
-            Selection.activeObject = newSettings;
+            UnityEditor.AssetDatabase.CreateAsset(newSettings, "Assets/Resources/SOArchitecture_Settings.asset");
+            UnityEditor.AssetDatabase.SaveAssets();
 
-            Debug.LogWarning("No SOArchitecture_Settings asset found! " +
-                "Created new one at asset root, feel free to move it wherever you please in your project.", newSettings);
+            UnityEditor.Selection.activeObject = newSettings;
+
+            Debug.LogWarning("No SOArchitecture_Settings asset found! Creating new one, ensure it's locatable by Resources", newSettings);
 
             return newSettings;
 #else
         throw new System.NullReferenceException();
 #endif
         }
+        #endregion
 
-        private const string AssetDatabaseSearchString = "t:SOArchitecture_Settings";
-        private const string DefaultNewSettingsLocation = "Assets\\";
-        private const string DefaultNewSettingsName = "SOArchitecture_Settings.asset";
-#endregion
+        public bool DrawEventGizmos { get { return _drawEventGizmos; } }
+        public string CodeGenerationTargetDirectory { get { return _codeGenerationTargetDirectory; } }
+        public bool CodeGenerationAllowOverwrite { get { return _codeGenerationAllowOverwrite; } }
+        public int DefaultCreateAssetMenuOrder { get { return _defualtCreateAssetMenuOrder; } }
+        public bool EnableDebug { get { return _enableDebug; } }
 
-        public string CodeGenerationTargetDirectory
-        {
-            get { return _codeGenerationTargetDirectory; }
-            set { _codeGenerationTargetDirectory = value; }
-        }
-
-        public bool CodeGenerationAllowOverwrite
-        {
-            get { return _codeGenerationAllowOverwrite; }
-            set { _codeGenerationAllowOverwrite = value; }
-        }
-
-        public int DefaultCreateAssetMenuOrder
-        {
-            get { return _defualtCreateAssetMenuOrder; }
-            set { _defualtCreateAssetMenuOrder = value; }
-        }
-
+        [SerializeField]
+        private bool _drawEventGizmos = true;
         [SerializeField]
         private string _codeGenerationTargetDirectory = "CODE_GENERATION";
-
         [SerializeField, Tooltip("Allow newly generated code files to overwrite existing ones")]
         private bool _codeGenerationAllowOverwrite = false;
-
         [SerializeField]
         private int _defualtCreateAssetMenuOrder = 120;
+        [SerializeField, Tooltip("Will enable features such as game events collecting stack traces.\nWill decrease performance in-editor.")]
+        private bool _enableDebug = true;
     }
 }
