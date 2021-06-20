5827b261ab2e0142d681950e44995e5af0364582
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 12:58:25 -0700

    reverted to only 1 generic arg

--- Assets/SO Architecture/Variables/DoubleVariable.cs
+++ Assets/SO Architecture/Variables/DoubleVariable.cs
@@ -1,16 +1,12 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
 
 namespace ScriptableObjectArchitecture
 {
-    [System.Serializable]
-    public class DoubleEvent : UnityEvent<double> { }
-
     [CreateAssetMenu(
         fileName = "DoubleVariable.asset",
         menuName = SOArchitecture_Utility.ADVANCED_VARIABLE_SUBMENU + "double",
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 8)]
-    public class DoubleVariable : BaseVariable<double, DoubleEvent>
+    public class DoubleVariable : BaseVariable<double>
     {
         public override bool Clampable { get { return true; } }
         protected override double ClampValue(double value)
