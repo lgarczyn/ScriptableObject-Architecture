4563c3fac56d3c66b52ffb05a69bfc1ac8928773
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 13:08:31 -0700

    added AudioOptions data

--- Assets/SO Architecture/References/AudioClipReference.cs
+++ Assets/SO Architecture/References/AudioClipReference.cs
@@ -7,5 +7,27 @@ namespace ScriptableObjectArchitecture
 	{
 	    public AudioClipReference() : base() { }
 	    public AudioClipReference(AudioClip value) : base(value) { }
+
+        /// <summary>
+        /// AudioClip options that indicate how this clip should be played.
+        /// </summary>
+        public AudioOptions Options
+        {
+            get => (_useConstant || _variable == null) 
+                ? AudioOptions.DefaultSFX : _variable.Options;
+
+            set
+            {
+                if (!_useConstant && _variable != null)
+                {
+                    _variable.Options = value;
+                }
+                else
+                {
+                    _useConstant = true;
+                    //myOptions = value; can't set default
+                }
+            }
+        }
 	}
 }
