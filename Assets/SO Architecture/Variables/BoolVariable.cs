edb5817a607e711560033122780f8143a8d5801e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 13:05:26 -0700

    Added Inversion behaviour
    can subscribe to get inverted value
    helper functions for inverted value

--- Assets/SO Architecture/Variables/BoolVariable.cs
+++ Assets/SO Architecture/Variables/BoolVariable.cs
@@ -1,16 +1,43 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ScriptableObjectArchitecture
 {
-    [System.Serializable]
-    public class BoolEvent : UnityEvent<bool> { }
-
     [CreateAssetMenu(
         fileName = "BoolVariable.asset",
         menuName = SOArchitecture_Utility.VARIABLE_SUBMENU + "bool",
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 5)]
-    public sealed class BoolVariable : BaseVariable<bool, BoolEvent>
+    public sealed class BoolVariable : BaseVariable<bool>
     {
-    }
+        private readonly List<Action<bool>> _invertedActions 
+            = new List<Action<bool>>();
+
+        public override object BaseValue
+        {
+            set
+            {
+                base.BaseValue = value;
+                RaiseInverted(!_value);
+            }
+        }
+        public void AddInvertedListener(Action<bool> action)
+        {
+            if (!_invertedActions.Contains(action))
+                _invertedActions.Add(action);
+        }
+        public void RemoveInvertedListener(Action<bool> action)
+        {
+            if (!_invertedActions.Contains(action))
+                _invertedActions.Remove(action);
+        }
+        public void RaiseInverted(bool invertedValue)
+        {
+            for (var i = _invertedActions.Count - 1; i >= 0; --i)
+                _invertedActions[i].Invoke(invertedValue);
+        }
+
+        public bool InvertedValue { get => !Value; }
+        public void InvertValue() => Value = !Value;
+    } 
 }
