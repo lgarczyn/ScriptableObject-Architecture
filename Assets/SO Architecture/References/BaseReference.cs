79d0fc4d9660097669bfd005d5b885b3a3ca4918
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-11-24 02:36:03 -0800

    Don't subscribe listeners if using a constant.

--- Assets/SO Architecture/References/BaseReference.cs
+++ Assets/SO Architecture/References/BaseReference.cs
@@ -14,6 +14,7 @@ namespace ScriptableObjectArchitecture
 
         [SerializeField]
         protected bool _useConstant = false;
+        public bool UseConstant { get => _useConstant; }
         [SerializeField]
         protected TBase _constantValue = default(TBase);
         [SerializeField]
@@ -57,22 +58,22 @@ namespace ScriptableObjectArchitecture
         }
         public void AddListener(IGameEventListener listener)
         {
-            if (_variable != null)
+            if (!(_variable == null || _useConstant))
                 _variable.AddListener(listener);
         }
         public void RemoveListener(IGameEventListener listener)
         {
-            if (_variable != null)
+            if (!(_variable == null || _useConstant))
                 _variable.RemoveListener(listener);
         }
         public void AddListener(System.Action action)
         {
-            if (_variable != null)
+            if (!(_variable == null || _useConstant))
                 _variable.AddListener(action);
         }
         public void RemoveListener(System.Action action)
         {
-            if (_variable != null)
+            if (!(_variable == null || _useConstant))
                 _variable.RemoveListener(action);
         }
         public override string ToString()
