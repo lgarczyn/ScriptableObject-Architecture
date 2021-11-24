80efd4e86fefa52f59a1e8cbe7b2974ef1335835
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-11-24 02:28:55 -0800

    Add InvertedValue and its own listeners.
    For those who want to subscribe to !myBool.

--- Assets/SO Architecture/Variables/BoolVariable.cs
+++ Assets/SO Architecture/Variables/BoolVariable.cs
@@ -13,32 +13,31 @@ namespace ScriptableObjectArchitecture
         public override bool IsInitializeable { get => !_readOnly; }
         private readonly List<Action<bool>> _invertedActions 
             = new List<Action<bool>>();
+            
+        public bool InvertedValue { get => !Value; }
 
         public void AddInvertedListener(Action<bool> action)
         {
             if (!_invertedActions.Contains(action))
                 _invertedActions.Add(action);
         }
-        public void RemoveAllInvertedListeners()
-        {
-            _invertedActions.Clear();
-        }
         public void RemoveInvertedListener(Action<bool> action)
         {
             _invertedActions.Remove(action);
         }
+        public override void RemoveAllListeners()
+        {
+            base.RemoveAllListeners();
+            _invertedActions.Clear();
+        }
         public override void Raise()
         {
             base.Raise();
-            RaiseInverted(!_value);
-        }
-        public void RaiseInverted(bool invertedValue)
-        {
+            var invertedValue = !_value;
             for (var i = _invertedActions.Count - 1; i >= 0; --i)
                 _invertedActions[i](invertedValue);
         }
 
-        public bool InvertedValue { get => !Value; }
         public void InvertValue() => Value = !Value;
     } 
 }
